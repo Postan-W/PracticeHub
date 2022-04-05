90636290bed7d03116cd1e0aca013c1a9ff781bf
15216 <15216802238@163.com>
2022-04-05 23:23:44 +0800

    Pytorch

--- demo_projects/pytorch/pytorch-tutorial/tutorials/01-basics/feedforward_neural_network/main.py
+++ demo_projects/pytorch/pytorch-tutorial/tutorials/01-basics/feedforward_neural_network/main.py
@@ -5,7 +5,9 @@ import torchvision.transforms as transforms
 
 
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+has_cuda = torch.cuda.is_available()
+print(has_cuda)
+device = torch.device('cuda' if has_cuda else 'cpu')
 
 # Hyper-parameters 
 input_size = 784
@@ -15,7 +17,8 @@ num_epochs = 5
 batch_size = 100
 learning_rate = 0.001
 
-# MNIST dataset 
+# MNIST dataset
+#改为本地的下载地址(在浏览器上打开文件夹)
 train_dataset = torchvision.datasets.MNIST(root='../../data', 
                                            train=True, 
                                            transform=transforms.ToTensor(),  
@@ -23,7 +26,8 @@ train_dataset = torchvision.datasets.MNIST(root='../../data',
 
 test_dataset = torchvision.datasets.MNIST(root='../../data', 
                                           train=False, 
-                                          transform=transforms.ToTensor())
+                                          transform=transforms.ToTensor(),
+                                          download=True)
 
 # Data loader
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
@@ -40,7 +44,7 @@ class NeuralNet(nn.Module):
         super(NeuralNet, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size) 
         self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, num_classes)  
+        self.fc2 = nn.Linear(hidden_size,num_classes)
     
     def forward(self, x):
         out = self.fc1(x)
@@ -52,7 +56,7 @@ model = NeuralNet(input_size, hidden_size, num_classes).to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
+optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
 
 # Train the model
 total_step = len(train_loader)
@@ -67,13 +71,25 @@ for epoch in range(num_epochs):
         loss = criterion(outputs, labels)
         
         # Backward and optimize
+        """
+        tensor的requires_grads为True，就会开始跟踪这个tensor上面的所有运算，
+        如果你做完运算后使用tensor.backward()，所有的梯度就会自动运算，tensor
+        的梯度将会累加到它的.grad属性里面去
+        """
+        """
+        损失函数loss是由模型的所有权重w经过一系列运算得到的，若某个w的requires_grads为True，
+        则w的所有上层参数（后面层的权重w）的.grad_fn属性中就保存了对应的运算，然后在使用
+        loss.backward()后，会一层层的反向传播计算每个w的梯度值，并保存到该w的.grad属性中
+        """
+        # 将参数tensor的梯度归0,即将tensor的.grad属性保存的值置0，因为这是上一个batch计算出来的
         optimizer.zero_grad()
-        loss.backward()
+        loss.backward()#计算参数tenor的梯度
+        #optimizer只负责通过梯度下降进行优化，而不负责产生梯度，梯度是tensor.backward()方法产生的
         optimizer.step()
         
         if (i+1) % 100 == 0:
             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+                   .format(epoch+1, num_epochs, i+1, total_step,loss.item()))
 
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
@@ -86,7 +102,7 @@ with torch.no_grad():
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+        correct += (predicted == labels).sum().item()#item()是得到只有一个元素的张量里面的元素值
 
     print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
 
